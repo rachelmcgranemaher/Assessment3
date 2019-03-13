0942509fd8c2643a475ebab51f05ab8a94f58b39
Rachel Mc Grane Maher <c15529443@BUS-3079-09.ict.ad.dit.ie>
2019-03-13 17:26:56 +0000

    Removed the comments to give the classes a cleaner look
    Updated the employee details class so the pssn was now 6 letters with a
    number at the end

--- RefactoringExercise2019/EmployeeSummaryDialog.java
+++ RefactoringExercise2019/EmployeeSummaryDialog.java
@@ -1,9 +1,3 @@
-/*
- * 
- * This is the summary dialog for displaying all Employee details
- * 
- * */
-
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -27,7 +21,7 @@ import javax.swing.table.DefaultTableModel;
 import net.miginfocom.swing.MigLayout;
 
 public class EmployeeSummaryDialog extends JDialog implements ActionListener {
-	// vector with all Employees details
+	
 	Vector<Object> allEmployees;
 	JButton back;
 	
@@ -46,29 +40,28 @@ public class EmployeeSummaryDialog extends JDialog implements ActionListener {
 		setVisible(true);
 
 	}
-	// initialise container
+	
 	public Container summaryPane() {
 		JPanel summaryDialog = new JPanel(new MigLayout());
 		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		JTable employeeTable;
 		DefaultTableModel tableModel;
-		// column center alignment
+		
 		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-		// column left alignment 
+		
 		DefaultTableCellRenderer leftRenderer = new DefaultTableCellRenderer();
 		Vector<String> header = new Vector<String>();
-		// header names
+	
 		String[] headerName = { "ID", "PPS Number", "Surname", "First Name", "Gender", "Department", "Salary",
 				"Full Time" };
-		// column widths
+	
 		int[] colWidth = { 15, 100, 120, 120, 50, 120, 80, 80 };
 		centerRenderer.setHorizontalAlignment(JLabel.CENTER);
 		leftRenderer.setHorizontalAlignment(JLabel.LEFT);
-		// add headers
+	
 		for (int i = 0; i < headerName.length; i++) {
 			header.addElement(headerName[i]);
-		}// end for
-		// construnct table and choose table model for each column
+		}
 		tableModel = new DefaultTableModel(this.allEmployees, header) {
 			public Class getColumnClass(int c) {
 				switch (c) {
@@ -82,16 +75,15 @@ public class EmployeeSummaryDialog extends JDialog implements ActionListener {
 					return Boolean.class;
 				default:
 					return String.class;
-				}// end switch
-			}// end getColumnClass
+				}
+			}
 		};
 
 		employeeTable = new JTable(tableModel);
-		// add header names to table
+		
 		for (int i = 0; i < employeeTable.getColumnCount(); i++) {
 			employeeTable.getColumn(headerName[i]).setMinWidth(colWidth[i]);
-		}// end for
-		// set alignments
+		}
 		employeeTable.getColumnModel().getColumn(0).setCellRenderer(leftRenderer);
 		employeeTable.getColumnModel().getColumn(4).setCellRenderer(centerRenderer);
 		employeeTable.getColumnModel().getColumn(6).setCellRenderer(new DecimalFormatRenderer());
@@ -110,7 +102,7 @@ public class EmployeeSummaryDialog extends JDialog implements ActionListener {
 		scrollPane.setBorder(BorderFactory.createTitledBorder("Employee Details"));
 		
 		return summaryDialog;
-	}// end summaryPane
+	}
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == back){
@@ -118,7 +110,7 @@ public class EmployeeSummaryDialog extends JDialog implements ActionListener {
 		}
 
 	}
-	// format for salary column
+	
 	static class DecimalFormatRenderer extends DefaultTableCellRenderer {
 		 private static final DecimalFormat format = new DecimalFormat(
 		 "\u20ac ###,###,##0.00" );
@@ -129,10 +121,10 @@ public class EmployeeSummaryDialog extends JDialog implements ActionListener {
 			Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 			 JLabel label = (JLabel) c;
 			 label.setHorizontalAlignment(JLabel.RIGHT);
-			 // format salary column
+			
 			value = format.format((Number) value);
 
 			return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-		}// end getTableCellRendererComponent
-	}// DefaultTableCellRenderer
-}// end class EmployeeSummaryDialog
+		}
+	}
+}
--- RefactoringExercise2019/SearchByIdDialog.java
+++ RefactoringExercise2019/SearchByIdDialog.java
@@ -1,9 +1,3 @@
-/*
- * 
- * This is the dialog for Employee search by ID
- * 
- * */
-
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
@@ -25,7 +19,7 @@ public class SearchByIdDialog extends JDialog implements ActionListener {
 	EmployeeDetails parent;
 	JButton search, cancel;
 	JTextField searchField;
-	// constructor for SearchByIdDialog 
+
 	public SearchByIdDialog(EmployeeDetails parent) {
 		setTitle("Search by Surname");
 		setModal(true);
@@ -40,9 +34,9 @@ public class SearchByIdDialog extends JDialog implements ActionListener {
 		setSize(500, 190);
 		setLocation(350, 250);
 		setVisible(true);
-	}// end SearchByIdDialog
+	}
 	
-	// initialize search container
+	
 	public Container searchPane() {
 		JPanel searchPanel = new JPanel(new GridLayout(3, 1));
 		JPanel textPanel = new JPanel();
@@ -69,28 +63,28 @@ public class SearchByIdDialog extends JDialog implements ActionListener {
 		searchPanel.add(buttonPanel);
 
 		return searchPanel;
-	}// end searchPane
+	}
 
-	// action listener for save and cancel button
+	
 	public void actionPerformed(ActionEvent e) {
-		// if option search, search for Employee
+		
 		if (e.getSource() == search) {
-			// try get correct valus from text field
+			
 			try {
 				Double.parseDouble(searchField.getText());
 				this.parent.searchByIdField.setText(searchField.getText());
-				// search Employee by ID
+				
 				this.parent.searchEmployeeById();
-				dispose();// dispose dialog
-			}// end try
+				dispose();
+			}
 			catch (NumberFormatException num) {
-				// display message and set colour to text field if entry is wrong
+				
 				searchField.setBackground(new Color(255, 150, 150));
 				JOptionPane.showMessageDialog(null, "Wrong ID format!");
-			}// end catch
-		}// end if
-		// else dispose dialog
+			}
+		}
+		
 		else if (e.getSource() == cancel)
 			dispose();
-	}// end actionPerformed
-}// end class searchByIdDialog
+	}
+}
